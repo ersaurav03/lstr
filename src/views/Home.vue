69d3670ad93ffdcaaf5c1cb300551950197e2636
ersaurav03 <er.saurav03@gmail.com>
2019-01-08 18:51:19 +0530

    work on about file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
 <Abc></Abc>
+<!-- <JSMPJ></JSMPJ> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Abc from './abc.vue';
+// import JSMPJ from './JSMPJ.vue'
 
 export default {
   name: 'home',
   components: {
   Abc
+  // JSMPJ
   }
 }
 </script>
