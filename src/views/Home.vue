5c9963a59222af56e3b4e05509deda4cf93a58dd
ersaurav03 <er.saurav03@gmail.com>
2019-01-08 18:44:04 +0530

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+<Abc></Abc>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Abc from './abc.vue';
+
+export default {
+  name: 'home',
+  components: {
+  Abc
+  }
+}
+</script>
---
+++ src/views/formm.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<!-- <h1 class="text-danger">JSMPJ</h1> -->
+		<form>
+			<div class="form-group">
+				<label for="exampleInputEmail1">Email address</label>
+				<input type="text" class="form-control" id="exampleInputEmail1" v-model="guestName"  placeholder="Enter email">
+			</div>
+			<button type="submit" class="btn btn-primary" @click="saveGuest">Submit</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	const STORAGE_KEY = "guest-storage"
+	
+	export default{
+		data:function(){
+         return{
+           guestName:"",
+           myGuest:[]
+         }
+		},
+	     created(){
+          this.myGuest=JSON.parse(localStorage.getItem(STORAGE_KEY || '[]'))
+
+          },
+		methods:{
+			saveGuest(){
+				this.myGuest.push({
+                guestName: this.guestName
+                }) 
+
+                this.guestName=""
+
+                localStorage.setItem(STORAGE_KEY, JSON.stringify(this.myGuest));
+			}
+		}
+	}
+</script>
