6b3f5736cdc8c3ca83b9dc3e68038124c4559b66
ersaurav03 <er.saurav03@gmail.com>
2019-01-09 12:44:38 +0530

    table type

--- src/views/display.vue
+++ src/views/display.vue
@@ -1,7 +1,28 @@
 <template>
 	<div>
-<h6 v-for="earr1 in earr">{{earr1}}</h6>
-<h6 v-for="parr1 in parr">{{parr1}}</h6>
+		<table>
+			<thead>
+<!-- 				<tr>
+					<th>ID</th><th>Password</th>
+				</tr> -->
+			</thead>
+			<tbody>
+			<tr>
+				<td>
+				<th scope="row">Email</th>
+				<tr v-for="earr1 in earr">{{earr1}}</tr>
+			</td>
+			
+			
+				<td>
+				<th scope="row">Passworrd</th> 
+				<tr v-for=" parr1 in parr">{{parr1}}</tr>
+				</td>
+			</tr>
+			</tbody>
+		</table>
+<!-- ID's<h6 v-for="earr1 in earr">{{earr1}}</h6>
+Password's:<h6 v-for="parr1 in parr">{{parr1}}</h6> -->
 </div>
 </template>
 
